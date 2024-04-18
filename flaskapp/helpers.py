525f0a242787e87ff59ba414cefe1db5691bab04
John <40076950+dataserver@users.noreply.github.com>
2024-04-18 12:16:33 -0300

    docstring

--- flaskapp/helpers.py
+++ flaskapp/helpers.py
@@ -7,19 +7,29 @@ from flask import Response
 from flask import current_app as app
 
 
-def filter_strftime(utc_dt_str: str, format=None) -> str:
-    """Convert stored string datetime from database
-    to format set in config timezone.
+def filter_strftime(iso_utc_dt_str: str, format: str | None = None) -> str:
+    """Formats a ISO-formatted UTC datetime string in the specified timezone and format.
 
     Args:
-        utc_dt_str (str): Datetime format
+        iso_utc_dt_str (str): The input datetime string in ISO format with UTC timezone.
+        format (str, optional): The output datetime format string.
+        If not provided, the default display datetime format from the application's configuration is used.
 
     Returns:
         str: Date string on format
+
+    Examples:
+        >>> app = {
+        ...     "TIMEZONE": "America/Los_Angeles",
+        ...     "DEFAULT_DISPLAY_DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S %Z"
+        ... }
+        >>> iso_utc_dt_str = "2023-04-18T12:05:00Z"
+        >>> formatted_dt_str = filter_strftime(iso_utc_dt_str)
+        >>> print(formatted_dt_str)  # Output: 2023-04-18 05:05:00 Pacific Daylight Time
     """
 
     # Parse the UTC datetime string and set the timezone to UTC
-    utc_dt = datetime.fromisoformat(utc_dt_str).replace(tzinfo=ZoneInfo("UTC"))
+    utc_dt = datetime.fromisoformat(iso_utc_dt_str).replace(tzinfo=ZoneInfo("UTC"))
 
     # Create the America/XYZ timezone object
     target_tz = ZoneInfo(app.config["TIMEZONE"])
@@ -31,7 +41,26 @@ def filter_strftime(utc_dt_str: str, format=None) -> str:
     return target_dt.strftime(format)
 
 
-def listdirs(rootdir):
+def listdirs(rootdir: str | Path):
+    """
+    Recursively lists all directories under the specified root directory.
+
+    Args:
+        rootdir (str or pathlib.Path): The root directory to start the search from.
+
+    Returns:
+        None: This function prints the directory paths to the console and does not return a value.
+
+    Examples:
+        >>> import pathlib
+        >>> rootdir = pathlib.Path("/path/to/root/directory")
+        >>> listdirs(rootdir)
+        /path/to/root/directory/subdir1
+        /path/to/root/directory/subdir2
+        /path/to/root/directory/subdir3/subsubdir1
+        /path/to/root/directory/subdir3/subsubdir2
+
+    """
     for path in Path(rootdir).iterdir():
         if path.is_dir():
             print(path)
--- flaskapp/models/helpers.py
+++ flaskapp/models/helpers.py
@@ -6,19 +6,26 @@ from flask import current_app as app
 # from flaskapp.database import db
 
 
-def iso_utc_to_tz(utc_dt_str: str) -> str:
-    """Convert stored string datetime from database
-    to format set in config timezone.
+def iso_utc_to_tz(iso_utc_dt_str: str) -> str:
+    """Formats a ISO-formatted UTC datetime string in the specified timezone and format.
 
     Args:
-        utc_dt_str (str): Datetime format
-
+        iso_utc_dt_str (str): The input datetime string in ISO format with UTC timezone.
     Returns:
         str: Date string on format
+
+    Examples:
+        >>> app = {
+        ...     "TIMEZONE": "America/Los_Angeles",
+        ...     "DEFAULT_DISPLAY_DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S %Z"
+        ... }
+        >>> iso_utc_dt_str = "2023-04-18T12:05:00Z"
+        >>> formatted_dt_str = filter_strftime(iso_utc_dt_str)
+        >>> print(formatted_dt_str)  # Output: 2023-04-18 05:05:00 Pacific Daylight Time
     """
 
     # Parse the UTC datetime string and set the timezone to UTC
-    utc_dt = datetime.fromisoformat(utc_dt_str).replace(tzinfo=ZoneInfo("UTC"))
+    utc_dt = datetime.fromisoformat(iso_utc_dt_str).replace(tzinfo=ZoneInfo("UTC"))
 
     # Create the America/XYZ timezone object
     target_tz = ZoneInfo(app.config["TIMEZONE"])
